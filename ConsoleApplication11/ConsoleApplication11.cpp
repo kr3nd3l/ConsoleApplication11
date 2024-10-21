bebb30d0f6477b11dbab770e59ae90820e0ad1d3
User <User@WIN-GDRLM5EKCOG>
2024-10-21 11:39:30 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApplication11/ConsoleApplication11.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+int reverseNumber(int num, int reversed = 0) {
+   
+    if (num == 0) {
+        return reversed;
+    }
+
+    
+    reversed = reversed * 10 + num % 10;
+
+    
+    return reverseNumber(num / 10, reversed);
+}
+
+int main() {
+    int number;
+
+    std::cout << "enter number: ";
+    std::cin >> number;
+
+    
+    bool isNegative = number < 0;
+    if (isNegative) {
+        number = -number;
+    }
+
+    int reversedNumber = reverseNumber(number);
+
+   
+    if (isNegative) {
+        reversedNumber = -reversedNumber;
+    }
+
+    std::cout << "reverse number: " << reversedNumber << std::endl;
+
+    return 0;
+}
